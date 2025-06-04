92d623a77e11c5f061c815566cbfccf62a5000ea
Mohammed Essam <mohammed.essam.10101@gmail.com>
2025-06-04 19:24:25 +0300

    Remove 'name' column from users table migration and add 'status' column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('status', ['pending_email_verification', 'pending_info', 'pending_nid', 'pending_approval', 'approved', 'rejected', 'suspended'])->default('pending_email_verification');
             $table->rememberToken();
             $table->timestamps();
         });
