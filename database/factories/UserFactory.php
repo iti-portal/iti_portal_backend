cc4aa816867a6baf5222918151e85ca11edbfcef
omarmorad <omorad5362@gmail.com>
2025-06-28 13:41:58 +0300

    feat(UserFactory): expand user status options to include suspended and rejected

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'status' => $this->faker->randomElement(['approved', 'pending']),
+            'status' => $this->faker->randomElement(['approved', 'pending','suspended','rejected']),
             'remember_token' => Str::random(10),
         ];
     }
