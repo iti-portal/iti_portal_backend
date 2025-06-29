2634b4c1fb32ca1d7f905319a94bbd96346fb707
Mohammed Essam <mohammed.essam.10101@gmail.com>
2025-06-29 19:56:28 +0300

    feat(AwardController): implement CRUD operations, requests and routes for Awards management

---
+++ app/Http/Requests/Awards/ChangeAwardImageRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Awards;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+class ChangeAwardImageRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:2048',
+        ];
+    }
+
+    /**
+     * Get custom messages for validator errors.
+     */
+    public function messages(): array
+    {
+        return [
+            'image.required' => 'Image is required',
+            'image.image' => 'The file must be an image.',
+            'image.mimes' => 'The image must be a file of type: jpeg, png, jpg.',
+            'image.max' => 'The image may not be greater than 2 MBs.',
+        ];
+    }
+}
