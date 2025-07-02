61d218a37b659164461d082e827c7712c047ce98
MahmoudEl-deeb <mahmoud_ahmed_eldeeb@outlook.com>
2025-07-02 09:27:19 +0300

    feat(UserProfileController): enhance user retrieval with additional filtering by branch, intake, and track

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -37,13 +37,51 @@ class UserProfileController extends Controller
         }
    }
    public function getAllItians(Request $request){
+    $user = $request->user();
     if(!$request->user()){
         return $this->respondWithError('unauthorized', 401);
     }
     try{
+        $branch = $user->profile->branch ?? null;
+        $intake = $user->profile->intake ?? null;
+        $track = $user->profile->track ?? null;
+
         $users = User::with('profile')
-        ->whereHas('roles', function ($query)
-        {$query->whereIn('name', ['student', 'alumni']);})->paginate(10);
+            ->where('id', '!=', $request->user()->id)
+            ->whereHas('roles', function ($query) {
+                $query->whereIn('name', ['student', 'alumni']);
+            })
+            ->when($branch && $intake && $track, function ($query) use ($branch, $intake, $track) {
+                $query->orderByRaw("CASE 
+                    WHEN EXISTS (
+                        SELECT 1 FROM user_profiles 
+                        WHERE user_profiles.user_id = users.id 
+                        AND user_profiles.track = ?
+                    ) THEN 0
+                    ELSE 1
+                END", [$track]);
+
+                $query->orderByRaw("CASE 
+                    WHEN EXISTS (
+                        SELECT 1 FROM user_profiles 
+                        WHERE user_profiles.user_id = users.id 
+                        AND user_profiles.intake = ?
+                    ) THEN 0
+                    ELSE 1
+                END", [$intake]);
+
+                $query->orderByRaw("CASE 
+                    WHEN EXISTS (
+                        SELECT 1 FROM user_profiles 
+                        WHERE user_profiles.user_id = users.id 
+                        AND user_profiles.branch = ?
+                    ) THEN 0
+                    ELSE 1
+                END", [$branch]);
+            })
+
+            ->get();
+
         return $this->respondWithSuccess(['users' => $users]);
         }catch(\Exception $e){
             $this->respondWithError("Something went wrong", 500);
