542ed7ce9f3d403eb4cae8841576bb9f80781412
omarmorad <omorad5362@gmail.com>
2025-07-07 13:25:48 +0300

    feat: Include ContactUs routes in API routing file

--- routes/api.php
+++ routes/api.php
@@ -23,6 +23,7 @@ require __DIR__. '/api/jobApplication.route.php';
 require __DIR__. '/api/awards.route.php';
 require __DIR__. '/api/certificates.route.php';
 require __DIR__ . '/api/chat.route.php';
+require __DIR__ . '/api/contactus.route.php';
 require __DIR__. '/api/statistics.route.php';
 
 use App\Http\Controllers\Auth\PasswordResetController;
