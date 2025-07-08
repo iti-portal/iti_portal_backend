4ce428888f08d447a16e81b1cd0511b60e593253
omarmorad <omorad5362@gmail.com>
2025-07-08 14:25:16 +0300

    feat(chat): add route for storing new conversations

--- routes/api/chat.route.php
+++ routes/api/chat.route.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Chat\MessageController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('conversations', [ConversationController::class, 'index']);
+    Route::post('conversations', [ConversationController::class, 'store']);
     Route::get('conversations/{conversation}/messages', [MessageController::class, 'index']);
     Route::post('messages', [MessageController::class, 'store']);
 });
